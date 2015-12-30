d14137f797c63b6cbfcea4c382dec38ed4091727
FedericoFdez <fa.fernandez@alumnos.upm.es>
2015-12-30 18:39:52 +0100

    Mejoras en el script de creación del escenario

--- creacion_escenario.py
+++ creacion_escenario.py
@@ -1,20 +1,31 @@
 import subprocess
+import time
 
-#Arrancar el escenario
-subprocess.call(["bin/prepare-p7-vm"])
-subprocess.call(["sudo", "vnx", "-f", "p7.xml", "-v", "--create"])
+NUM_SERVIDORES = 4
 
-#Configuracion de servidores de disco (nas)
-subprocess.call(["sudo", "lxc-attach", "-n", "nas1", "--", "gluster", "peer", "probe", "10.1.3.21"])
-subprocess.call(["sudo", "lxc-attach", "-n", "nas1", "--","gluster", "peer", "probe", "10.1.3.22"])
-subprocess.call(["sudo", "lxc-attach", "-n", "nas1", "--","gluster", "peer", "probe", "10.1.3.23"])
-subprocess.call(["sudo", "lxc-attach", "-n", "nas1", "--","gluster", "volume", "create", "nas", "replica", "3", "10.1.3.21:/nas", "10.1.3.22:/nas", "10.1.3.23:/nas", "force"])
-subprocess.call(["sudo", "lxc-attach", "-n", "nas1", "--","gluster", "volume", "start", "nas"])
-subprocess.call(["sudo", "lxc-attach", "-n", "nas1", "--","gluster", "volume", "info"])
+def run(machine, command):
+    args = ["sudo", "lxc-attach", "-n", machine, "--"] + command
+    if subprocess.call(args) != 0:
+        print "ERROR when running command: " + ''.join(args)
 
-#Configuracion del montaje desde servidores web (www)
-num_servidores = 4
-for i in range(num_servidores):
-	name = "s" + str(i + 1)
-	subprocess.call(["sudo", "lxc-attach", "-n", name, "--", "mkdir", "/mnt/nas"])
-	subprocess.call(["sudo", "lxc-attach", "-n", name, "--", "mount", "-t", "glusterfs", "10.1.3.21:/nas", "/mnt/nas"])
+def main():
+    #Arrancar el escenario
+    subprocess.call(["bin/prepare-p7-vm"])
+    subprocess.call(["sudo", "vnx", "-f", "p7.xml", "-v", "--create"])
+    time.sleep(2)
+    #Configurar los servidores de disco (NAS)
+    run("nas1", ["gluster", "peer", "probe", "10.1.3.21"])
+    run("nas1", ["gluster", "peer", "probe", "10.1.3.22"])
+    run("nas1", ["gluster", "peer", "probe", "10.1.3.23"])
+    run("nas1", ["gluster", "volume", "create", "nas", "replica", "3", "10.1.3.21:/nas", "10.1.3.22:/nas", "10.1.3.23:/nas", "force"])
+    run("nas1", ["gluster", "volume", "start", "nas"])
+    run("nas1", ["gluster", "volume", "info"])
+
+    #Configurar los servidores web (WWW)
+    for i in range(NUM_SERVIDORES):
+        name = "s" + str(i + 1)
+        run(name, ["mkdir", "/mnt/nas"])
+        run(name, ["mkdir", "mount", "-t", "glusterfs", "10.1.3.21:/nas", "/mnt/nas"])
+
+if __name__ == "__main__":
+    main()
